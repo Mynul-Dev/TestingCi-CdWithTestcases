0e168a7ff273da90c906c3e88914e498cc36b00b
mynul-dev <homeexercise8@gmail.com>
2023-10-18 13:25:21 +0600

    added new text

--- App.tsx
+++ App.tsx
@@ -53,6 +53,7 @@ export default () => {
           <Image source={{uri: image}} style={styles.image} />
         </View>
         <View style={styles.userInfoContainer}>
+          <Text>Hello</Text>
           <Text>
             {firstName} {lastName}
           </Text>
